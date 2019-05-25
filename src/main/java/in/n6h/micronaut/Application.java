10832ceb0b3e0e62ee63224346d447a2e4ad579a
Karanbir Singh <krnbr@live.in>
2019-05-25 22:14:32 +0530

    init ms-micro-naut

---
+++ src/main/java/in/n6h/micronaut/Application.java
@@ -0,0 +1,10 @@
+package in.n6h.micronaut;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Micronaut.run(Application.class);
+    }
+}
